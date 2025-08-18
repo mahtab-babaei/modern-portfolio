6751e961c5f5f6499a15e894cf73b0072cbc3a78
Mahtab <mahtaab.babaei@gmail.com>
2025-08-18 13:07:28 +0330

    Fix syntax error

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -268,15 +268,14 @@ export function World(props: WorldProps) {
       />
       <Globe {...props} />
       <OrbitControls
-        enablePan={false)
+        enablePan={false}
         enableZoom={false}
         minDistance={cameraZ}
         maxDistance={cameraZ}
         autoRotateSpeed={1}
         autoRotate={true}
         minPolarAngle={Math.PI / 3.5}
-        maxPolarAngle={Math.PI - Math.PI / 3}
-      />
+        maxPolarAngle={Math.PI - Math.PI / 3}/>
     </Canvas>
   );
 }
